6cb5f20df65a7830845817628801258959f8a8d4
enPiTT142019 <f216053@ed.socu.ac.jp>
2019-10-17 11:51:16 +0900

    Initial commit

---
+++ app/src/main/java/com/socu/enpit/sssforshop/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.socu.enpit.sssforshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
