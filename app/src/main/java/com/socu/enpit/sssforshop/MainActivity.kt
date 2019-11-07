ccef638edf88889037be8ac7ee6c695806de76fc
enPiTT142019 <f216053@ed.socu.ac.jp>
2019-11-07 12:47:32 +0900

    ニフクラAPIキーの追加

--- app/src/main/java/com/socu/enpit/sssforshop/MainActivity.kt
+++ app/src/main/java/com/socu/enpit/sssforshop/MainActivity.kt
@@ -3,6 +3,7 @@ package com.socu.enpit.sssforshop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nifcloud.mbaas.core.NCMB
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //**************** APIキーの設定とSDKの初期化 **********************
+        NCMB.initialize(this, "d33534c7daf258e354c67e100f9923e1be23c347779ef8379e7f8596271ccc29", "f0354f95aba607910c5cb18b8d74d8df858880316963c04ecb9d07f66aef05b2")
 
         // ログインボタンを押したときの処理
         loginButton.setOnClickListener {
