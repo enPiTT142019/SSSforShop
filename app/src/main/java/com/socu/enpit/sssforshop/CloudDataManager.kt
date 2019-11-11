8f6bd64123d96c1565f2a48e3ee75cf3fd933d37
enPiTT142019 <f216053@ed.socu.ac.jp>
2019-11-11 16:23:46 +0900

    何でもない変更

--- app/src/main/java/com/socu/enpit/sssforshop/CloudDataManager.kt
+++ app/src/main/java/com/socu/enpit/sssforshop/CloudDataManager.kt
@@ -7,20 +7,23 @@ object CloudDataManager {
     private const val CLASS_NAME_EDIT_SCREEN = "EditScreen"
     private const val KEY_ACCOUNT_USER_NAME: String = "AccountUserName"
     private const val KEY_SHOP_NAME: String = "ShopName"
+
     private var accountUserName: String? = null
+
     fun setAccountUserName(name: String) {
         accountUserName = name
     }
-    fun setShopName(name: String) {
+    private fun saveStringData(className: String, key: String, data: String)
+    {
         if (accountUserName == null) return
-        val query = NCMBQuery<NCMBObject>(CLASS_NAME_EDIT_SCREEN)
+        val query = NCMBQuery<NCMBObject>(className)
         query.whereEqualTo(KEY_ACCOUNT_USER_NAME, accountUserName)
         query.setLimit(1)
         val list: List<NCMBObject> = query.find()
         if (list.isEmpty()) {
-            val obj = NCMBObject(CLASS_NAME_EDIT_SCREEN)
+            val obj = NCMBObject(className)
             obj.put(KEY_ACCOUNT_USER_NAME, accountUserName)
-            obj.put(KEY_SHOP_NAME, name)
+            obj.put(key, data)
             obj.saveInBackground { e ->
                 if (e != null) {
                     // 保存に失敗した場合の処理
@@ -31,7 +34,7 @@ object CloudDataManager {
             return
         }
         val obj = list[0]
-        obj.put(KEY_SHOP_NAME, name)
+        obj.put(key, data)
         obj.saveInBackground { e ->
             if (e != null) {
                 // 保存に失敗した場合の処理
@@ -39,6 +42,9 @@ object CloudDataManager {
                 // 保存に成功した場合の処理
             }
         }
+    }
+    fun setShopName(name: String) {
+        saveStringData(CLASS_NAME_EDIT_SCREEN, KEY_SHOP_NAME, name)
     }
     fun getShopName(): String {
         if (accountUserName == null) return ""
@@ -61,5 +67,9 @@ object CloudDataManager {
         }
         val obj = list[0]
         return obj.getString(KEY_SHOP_NAME)
+    }
+    fun setNewsText(text: String)
+    {
+
     }
 }
