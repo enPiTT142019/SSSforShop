50f912058a8889e94ef508d018c2aa777fa63aa1
enPiTT142019 <f216053@ed.socu.ac.jp>
2019-11-14 10:49:01 +0900

    CloudDataManagerの保守性を高めた

--- app/src/main/java/com/socu/enpit/sssforshop/CloudDataManager.kt
+++ app/src/main/java/com/socu/enpit/sssforshop/CloudDataManager.kt
@@ -4,37 +4,45 @@ import com.nifcloud.mbaas.core.NCMBObject
 import com.nifcloud.mbaas.core.NCMBQuery
 
 object CloudDataManager {
-    private const val CLASS_NAME_EDIT_SCREEN = "EditScreen"
-    private const val KEY_ACCOUNT_USER_NAME: String = "AccountUserName"
-    private const val KEY_SHOP_NAME: String = "ShopName"
+    // 全店舗共通のクラス
+    private const val CLASS_SHARE_SHOP_INFO = "ShopInfo"
+
+    // 店舗ごとに存在するクラス
+    private const val CLASS_EACH_NEWS = "_News"
+    private const val CLASS_EACH_GROCERIES = "_Groceries"
+    private const val CLASS_EACH_REQUEST = "_Request"
+
+    // データの列
+    private const val KEY_USER_NAME: String = "userName"
+    private const val KEY_SHOP_NAME: String = "shopName"
+    private const val KEY_TITLE: String = "title"
+    private const val KEY_CONTENTS: String = "contents"
+    private const val KEY_IMAGE_NAME: String = "imageName"
 
     private var accountUserName: String? = null
+
+    private class KeyAndData constructor(k: String, d:String) {
+        val key: String = k
+        val data: String = d
+    }
 
     fun setAccountUserName(name: String) {
         accountUserName = name
     }
-    private fun saveStringData(className: String, key: String, data: String)
-    {
-        if (accountUserName == null) return
+    private fun getStringData(className: String, findKads: List<KeyAndData>, getKey: String): String? {
         val query = NCMBQuery<NCMBObject>(className)
-        query.whereEqualTo(KEY_ACCOUNT_USER_NAME, accountUserName)
+        for (kad in findKads) query.whereEqualTo(kad.key, kad.data)
         query.setLimit(1)
         val list: List<NCMBObject> = query.find()
         if (list.isEmpty()) {
-            val obj = NCMBObject(className)
-            obj.put(KEY_ACCOUNT_USER_NAME, accountUserName)
-            obj.put(key, data)
-            obj.saveInBackground { e ->
-                if (e != null) {
-                    // 保存に失敗した場合の処理
-                } else {
-                    // 保存に成功した場合の処理
-                }
-            }
-            return
+            return null
         }
         val obj = list[0]
-        obj.put(key, data)
+        return obj.getString(getKey)
+    }
+    private fun addStringData(className: String, insertKads: List<KeyAndData>) {
+        val obj = NCMBObject(className)
+        for (kad in insertKads)  obj.put(kad.key, kad.data)
         obj.saveInBackground { e ->
             if (e != null) {
                 // 保存に失敗した場合の処理
@@ -43,30 +51,43 @@ object CloudDataManager {
             }
         }
     }
-    fun setShopName(name: String) {
-        saveStringData(CLASS_NAME_EDIT_SCREEN, KEY_SHOP_NAME, name)
-    }
-    fun getShopName(): String {
-        if (accountUserName == null) return ""
-        val query = NCMBQuery<NCMBObject>(CLASS_NAME_EDIT_SCREEN)
-        query.whereEqualTo(KEY_ACCOUNT_USER_NAME, accountUserName)
-        query.setLimit(1)
+    private fun overwriteStringData(className: String, findKads: List<KeyAndData>, insertKads: List<KeyAndData>): Boolean {
+        val query = NCMBQuery<NCMBObject>(className)
+        for (kad in findKads) query.whereEqualTo(kad.key, kad.data)
+       query.setLimit(1)
         val list: List<NCMBObject> = query.find()
         if (list.isEmpty()) {
-            val obj = NCMBObject(CLASS_NAME_EDIT_SCREEN)
-            obj.put(KEY_ACCOUNT_USER_NAME, accountUserName)
-            obj.put(KEY_SHOP_NAME, accountUserName)
-            obj.saveInBackground { e ->
-                if (e != null) {
-                    // 保存に失敗した場合の処理
-                } else {
-                    // 保存に成功した場合の処理
-                }
-            }
-            return accountUserName!!
+            return false
         }
         val obj = list[0]
-        return obj.getString(KEY_SHOP_NAME)
+        for (kad in insertKads) obj.put(kad.key, kad.data)
+        obj.saveInBackground { e ->
+            if (e != null) {
+                // 保存に失敗した場合の処理
+            } else {
+                // 保存に成功した場合の処理
+            }
+        }
+        return true
+    }
+    fun setShopName(name: String) {
+        val findKads = listOf(KeyAndData(KEY_USER_NAME, accountUserName!!))
+        val insertKads = listOf(KeyAndData(KEY_SHOP_NAME, name))
+        val success = overwriteStringData(CLASS_SHARE_SHOP_INFO, findKads, insertKads)
+        if (!success)
+        {
+            val kads = arrayListOf(findKads, insertKads).flatten()
+            addStringData(CLASS_SHARE_SHOP_INFO, kads)
+        }
+    }
+    fun getShopName(): String {
+        val findKads = listOf(KeyAndData(KEY_USER_NAME, accountUserName!!))
+        val ret = getStringData(CLASS_SHARE_SHOP_INFO, findKads, KEY_SHOP_NAME)
+        if (ret != null) return ret
+        val insertKads = listOf(KeyAndData(KEY_SHOP_NAME, accountUserName!!))
+        val kads = arrayListOf(findKads, insertKads).flatten()
+        addStringData(CLASS_SHARE_SHOP_INFO, kads)
+        return accountUserName!!
     }
     fun setNewsText(text: String)
     {
