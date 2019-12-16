de9e5ce4bd2eab92c82ff2d7e86fb7272f3094ff
enPiT-P13 <f217040@ed.socu.ac.jp>
2019-12-16 13:40:01 +0900

    edit_activity picture

--- app/src/main/java/com/socu/enpit/sssforshop/MenuAdapter.kt
+++ app/src/main/java/com/socu/enpit/sssforshop/MenuAdapter.kt
@@ -60,15 +60,15 @@ class MenuAdapter(
 
         // reference: Y.A.M の 雑記帳: SimpleDateFormat ではなく android.text.format.DateFormat を使おう - http://bit.ly/2OybKLu
         //holder.tvCreatedAt.text = DateFormat.format("yyyy/MM/dd kk:mm:ss", mItems[position].mCreatedAt).toString()
-        holder.btRemoveItem?.setOnClickListener(){
+       /* holder.btRemoveItem?.setOnClickListener(){
             removeItem(position)
-        }
+        }*/
     }
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val title:       TextView    = view.titleText
         val contents:  TextView    = view.contentsText
         val date: TextView = view.dateText
-        val btRemoveItem: Button? = view.deleteButton
+       // val btRemoveItem: Button? = view.deleteButton
     }
 }
--- app/src/main/java/com/socu/enpit/sssforshop/NewsAdapter.kt
+++ app/src/main/java/com/socu/enpit/sssforshop/NewsAdapter.kt
@@ -56,17 +56,16 @@ class NewsAdapter(
 
         // reference: Y.A.M の 雑記帳: SimpleDateFormat ではなく android.text.format.DateFormat を使おう - http://bit.ly/2OybKLu
         //holder.tvCreatedAt.text = DateFormat.format("yyyy/MM/dd kk:mm:ss", mItems[position].mCreatedAt).toString()
-        holder.btRemoveItem?.setOnClickListener(){
-            //removese.play(soundResId, 1.0f, 1.0f, 0, 0, 1.0f)
+        /*holder.btRemoveItem?.setOnClickListener(){
             removeItem(position)
-        }
+        }*/
     }
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val title:       TextView    = view.titleText
         val contents:  TextView    = view.contentsText
         val date: TextView = view.dateText
-        val btRemoveItem: Button? = view.deleteButton
+       // val btRemoveItem: Button? = view.deleteButton
     }
 
     /*override fun onResume() {
